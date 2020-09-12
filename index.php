ea2f372210ee296c77c67859fb28243d4cac1c62
simoji ryuya <simojiryuya@gmail.com>
2020-09-13 00:21:32 +0900

    #3 未入力時の対応

--- index.php
+++ index.php
@@ -1,70 +1,81 @@
-<?php
+<?php 
 var_dump($_POST);
+  $errors = array();
 
-$page_flag = 0;
-
-if( !empty($_POST['btn_confirm'])) {
-  $page_flag = 1;
-} elseif( !empty($_POST['btn_submit']) ) {
-  $page_flag = 1;
+  if(isset($_POST['btn_submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    if($name === ""){
+        $errors['name'] = "名前が入力されていません。";
+        var_dump($errors['name']);
+    }
+    if($email === ""){
+        $errors['email'] = "メールアドレスが入力されていません。";
+        var_dump($errors['email']);
+    }
+    if($message === ""){
+        $errors['message'] = "内容が入力されていません。";
+        var_dump($errors['message']);
+    }
 }
-?>
 
+?> 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="index.css">
-  <title>Document</title>
+  <link rel="stylesheet" href="stylesheet.css">
+  <title>お問い合わせ</title>
 </head>
 <body>
+  <header>
+    <h1 id="title">お問い合わせ</h1>
+  </header>
+  <main>
 
-    <form method="post" action="Input_confirmation.php">
-      <table id="containerTop">
-        <tr>
-          <th></th>
-          <td>
-            <p id="secondTitle">お問い合わせフォーム</p>
-          </td>
-        </tr>
-        <tr>
-          <th>
-            氏名:
-          </th>
-          <td>
-            <input id="inp" type="text" name="your_name">
-          </td>
-        </tr>
-        <tr>
-          <th>
-            メールアドレス:
-          </th>
-          <td>
-            <input id="inp" type="text" name="email" value="">
-          </td>
-        </tr>
-        <tr>
-          <th>
-            お問い合わせ内容:
-          </th>
-          <td>
-            <textarea name="naiyou" id="area" cols="30" rows="10"></textarea>
-          </td>
-        </tr>
-        <tr>
-          <th>
-            <!-- 「確認」を合わせるためにthタグ内は空 -->
-          </th>
-          <td>
-            <input type="submit" name="btn_submit" value="確認">
-          </td>
-        </tr>
-      </table>
-    </form>
+    <p id="second_title">*お問い合わせフォーム*</p>
+      <form method="post" action="" >
 
+        <div class="item">
+        <?php 
+            if(isset($_POST['name'])){
+              echo '<p id="error">';
+              echo $errors['name'];
+              echo '</p>';
+            }
+            ?>
+          <label for="your_name">氏名:</label> <input id="your_name" type="text" name="name">
+        </div>
 
-  
+        <div class="item">
+        <?php 
+            if(isset($_POST['email'])){
+              echo '<p id="error">';
+              echo $errors['email'];
+              echo '</p>';
+            }
+            ?>
+          <label for="email">メールアドレス:</label> <input id="email" type="text" name="email">
+        </div>
+
+        <div class="item">
+        <?php 
+            if(isset($_POST['message'])){
+              echo '<p id="error">';
+              echo $errors['message'];
+              echo '</p>';
+            }
+            ?>
+          <label for="message">お問い合わせ内容:</label><textarea id="message" name="message" id="" cols="30" rows="10"></textarea>
+        </div>
+
+        <div class="btn-area">
+          <input type="submit" name="btn_submit" value="確認する">
+        </div>
+      </form>
+  </main>
 </body>
 </html>
